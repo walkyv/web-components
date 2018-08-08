183062e5baa856c3ea186cd6462a4a3ef3be0c40
Elias Mason <eliasjmason@gmail.com>
2018-08-08 15:26:06 -0700

    Add js to buttons

--- toggle/markup-kit/js/main.js
+++ toggle/markup-kit/js/main.js
@@ -1,4 +1,28 @@
 'use strict';
-(function() {
-  // do something
-})();
+(function(w, doc) {
+  const toggles = doc.querySelectorAll('.pe-toggle');
+
+  function toggleAriaChecked(checked, target) {
+    if (checked === 'false') {
+      target.setAttribute('aria-checked', 'true');
+    } else {
+      target.setAttribute('aria-checked', 'false');
+    }
+  }
+
+  function toggleValue(checked, target) {
+    target.value = checked;
+  }
+
+  toggles.forEach(toggle => {
+    const button = toggle.querySelector('button');
+
+    button.addEventListener('click', event => {
+      const target = event.currentTarget,
+        isChecked = target.getAttribute('aria-checked');
+
+      toggleValue(isChecked, target);
+      toggleAriaChecked(isChecked, target);
+    });
+  });
+})(window, document);
