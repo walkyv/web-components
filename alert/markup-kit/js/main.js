1cd0f3684bdc61f30f422873515940a058bb684c
Elias Mason <eliasjmason@gmail.com>
2018-08-29 10:27:45 -0700

    convert to modular use strict

--- alert/markup-kit/js/main.js
+++ alert/markup-kit/js/main.js
@@ -1,16 +1,16 @@
-'use strict';
-(function() {
+(function(w, doc) {
+  'use strict';
   // do something
   const closeButton = document.getElementById('closeAlert'),
     alert = document.getElementById('alert');
 
   closeButton.addEventListener('click', event => {
-      alert.classList.remove('slideInDown');
-      alert.classList.add('slideOutDown');
+    alert.classList.remove('slideInDown');
+    alert.classList.add('slideOutDown');
   });
 
   if (alert.attributes.important) {
     closeButton.focus();
   }
 
-})();
+})(window, document);
