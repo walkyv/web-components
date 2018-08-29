65b8b39d47271c0f010cc1f541bd6cdf1ec2287e
Elias Mason <eliasjmason@gmail.com>
2018-08-29 11:29:46 -0700

    Updates scripting to match markup Also adds hiding, focus management

--- alert/markup-kit/js/dist/main.js
+++ alert/markup-kit/js/dist/main.js
@@ -1,16 +1,30 @@
 'use strict';
 
-(function () {
+(function (w, doc) {
+  'use strict';
   // do something
-  var closeButton = document.getElementById('closeAlert'),
-      alert = document.getElementById('alert');
 
-  closeButton.addEventListener('click', function (event) {
+  var alertTrigger = doc.querySelector('[data-action="trigger-alert"]'),
+      alert = doc.querySelector('[data-alert]'),
+      alertBody = alert.querySelector('[data-alert-body'),
+      alertClose = alert.querySelector('[data-action="close-alert"]');
+
+  alertTrigger.addEventListener('click', function () {
+    alertBody.hidden = false;
+    alert.classList.add('slideInDown');
+    alert.classList.remove('slideOutDown');
+  });
+
+  alertClose.addEventListener('click', function () {
     alert.classList.remove('slideInDown');
     alert.classList.add('slideOutDown');
+    setTimeout(function () {
+      alertBody.hidden = true;
+    }, 100);
+    alertTrigger.focus();
   });
 
   if (alert.attributes.important) {
-    closeButton.focus();
+    alertClose.focus();
   }
-})();
+})(window, document);
--- alert/markup-kit/js/main.js
+++ alert/markup-kit/js/main.js
@@ -1,16 +1,28 @@
 (function(w, doc) {
   'use strict';
   // do something
-  const closeButton = doc.getElementById('closeAlert'),
-    alert = doc.getElementById('alert');
+  const alertTrigger = doc.querySelector('[data-action="trigger-alert"]'),
+    alert = doc.querySelector('[data-alert]'),
+    alertBody = alert.querySelector('[data-alert-body'),
+    alertClose = alert.querySelector('[data-action="close-alert"]');
 
-  closeButton.addEventListener('click', () => {
+  alertTrigger.addEventListener('click', function () {
+    alertBody.hidden = false;
+    alert.classList.add('slideInDown');
+    alert.classList.remove('slideOutDown');
+  });
+
+  alertClose.addEventListener('click', () => {
     alert.classList.remove('slideInDown');
     alert.classList.add('slideOutDown');
+    setTimeout(() => {
+      alertBody.hidden = true;
+    }, 100);
+    alertTrigger.focus();
   });
 
   if (alert.attributes.important) {
-    closeButton.focus();
+    alertClose.focus();
   }
 
 })(window, document);
