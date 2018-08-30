190e341a734a66489c8f0a77febc4fb163f8ace8
David O'Dey <dodey@icloud.com>
2018-08-30 10:55:15 -0700

    add basic functionality for adding files and rendering progress

--- file-uploader/markup-kit/js/dist/main.js
+++ file-uploader/markup-kit/js/dist/main.js
@@ -3,15 +3,40 @@
 (function () {
   var attachBtn = document.getElementById('attachFiles'),
       modal = document.querySelector('upload-modal'),
-      uploadInfo = document.getElementById('info');
+      uploadInfo = document.getElementById('info'),
+      realUploadInput = document.querySelector('input[type="file"]'),
+      fileArr = [];
 
-  console.log(uploadInfo);
   attachBtn.addEventListener('click', function (event) {
+    realUploadInput.click();
+  });
+
+  realUploadInput.addEventListener('change', function (event) {
+    // display wide view and action buttons
     if (modal.footer !== true) {
       modal.footer = true;
       uploadInfo.style.display = 'block';
-    } else {
-      return false;
     }
+
+    // create a map to loop over all the files
+    var map = new Map(Object.entries(event.srcElement.files));
+
+    // render progress
+    var target = document.querySelector('.pe-progress-container'),
+        uploadTitle = document.querySelector('.upload-title');
+
+    // loop over map and render individual progress
+    map.forEach(function (file, index) {
+      var div = document.createElement('DIV'),
+          html = ['<div class="group">', '<div class="indicator">', '<img src="./icons/indicator.png" alt="progress" />', '</div>', '<div class="text">', '<strong>', file.name, '</strong>', '<p class="info">0 MB / ', file.size, ' MB', '</p>', '</div>', '</div>', '<div class="upload-actions">', '<button class="pe-icon--btn">', '<svg focusable="false" class="pe-icon--remove-sm-24" aria-label="remove file" role="img" >', '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#remove-sm-24"></use>', '</svg>', '</button>', '</div>'].join('');
+
+      div.classList.add('progress');
+      target.appendChild(div);
+      div.innerHTML = html;
+      fileArr.push(file);
+    });
+
+    // update the title to show total progress
+    uploadTitle.innerHTML = "Uploading  (0 done, " + fileArr.length + " in progress)";
   });
 })();
--- file-uploader/markup-kit/js/main.js
+++ file-uploader/markup-kit/js/main.js
@@ -2,16 +2,64 @@
 (function() {
   const attachBtn = document.getElementById('attachFiles'),
     modal = document.querySelector('upload-modal'),
-    uploadInfo = document.getElementById('info');
+    uploadInfo = document.getElementById('info'),
+    realUploadInput = document.querySelector('input[type="file"]'),
+    fileArr = [];
 
-  console.log(uploadInfo)
   attachBtn.addEventListener('click', event => {
+    realUploadInput.click();
+  });
+
+  realUploadInput.addEventListener('change', event=> {
+    // display wide view and action buttons
     if (modal.footer !== true) {
       modal.footer = true;
       uploadInfo.style.display = 'block';
-    } else {
-      return false
     }
+
+    // create a map to loop over all the files
+    const map = new Map(Object.entries(event.srcElement.files));
+
+    // render progress
+    const target = document.querySelector('.pe-progress-container'),
+      uploadTitle = document.querySelector('.upload-title');
+
+    // loop over map and render individual progress
+    map.forEach((file, index) => {
+      const div = document.createElement('DIV'),
+        html = [
+          '<div class="group">',
+            '<div class="indicator">',
+              '<img src="./icons/indicator.png" alt="progress" />',
+            '</div>',
+            '<div class="text">',
+              '<strong>',
+                file.name,
+              '</strong>',
+              '<p class="info">0 MB / ',
+                file.size,
+                ' MB',
+              '</p>',
+            '</div>',
+          '</div>',
+          '<div class="upload-actions">',
+            '<button class="pe-icon--btn">',
+              '<svg focusable="false" class="pe-icon--remove-sm-24" aria-label="remove file" role="img" >',
+                '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#remove-sm-24"></use>',
+              '</svg>',
+            '</button>',
+          '</div>'
+        ].join('');
+
+      div.classList.add('progress');
+      target.appendChild(div);
+      div.innerHTML = html;
+      fileArr.push(file);
+    });
+
+    // update the title to show total progress
+    uploadTitle.innerHTML = "Uploading  (0 done, " + fileArr.length + " in progress)"
   })
+
 })();
 
