0792bb41c4c27122adbd2599b2cb0dc3a3fbf710
Elias Mason <eliasjmason@gmail.com>
2018-09-04 09:34:53 -0700

    Init web component from spec kit

---
+++ alert/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+class Example extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    // shadow dom
+    const shadow = this.attachShadow({
+        mode: 'open'
+      }),
+
+    currentDoc = document.querySelector('link[href$="index.html"]').import;
+    let template = currentDoc.querySelector('#template');
+    const clone = document.importNode(template.content, true);
+
+    shadow.appendChild(clone);
+    // set attributes
+
+    const titleText = this.getAttribute('title'),
+          titleContainer = shadow.getElementById('titleText');
+
+    titleContainer.innerHTML = titleText;
+    }
+
+}
+customElements.define('example-component', Example);
