73d4d131da4e733bb73888d7ca4db55f616bdf3b
David O'Dey <dodey@icloud.com>
2018-10-24 08:43:37 -0700

    refactored code to make it more maintainable

--- loading-indicator/js/dist/main.js
+++ loading-indicator/js/dist/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
 (function () {
-  // do something
+
+  function showSpinner(event) {
+    var liveregion = document.querySelector('#liveregion'),
+        template = document.querySelector('#loading-template').innerHTML;
+
+    document.querySelector('#loaded-content').innerHTML = "";
+    liveregion.innerHTML = template;
+
+    setTimeout(function () {
+      document.querySelector('#spinner-icon').setAttribute('aria-label', 'Loaded!');
+      document.querySelector('#loading-text').innerHTML = "Loaded!";
+    }, 2000);
+
+    setTimeout(function () {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'loaded-content.html', true);
+      request.onload = function (event) {
+        if (request.status >= 200 && request.status < 400) {
+          var resp = request.responseText;
+          document.querySelector('#loaded-content').innerHTML = resp;
+          document.querySelector('#loaded-content title').remove();
+        }
+      };
+      request.send();
+      liveregion.innerHTML = "";
+    }, 2500);
+  }
+
+  window.addEventListener('load', function (event) {
+    var button = document.querySelector('#load-page');
+    button.addEventListener('click', showSpinner);
+  });
 })();
--- loading-indicator/js/main.js
+++ loading-indicator/js/main.js
@@ -1,4 +1,36 @@
 'use strict';
 (function() {
-  // do something
+
+  function showSpinner (event) {
+    const liveregion = document.querySelector('#liveregion'),
+      template = document.querySelector('#loading-template').innerHTML;
+
+    document.querySelector('#loaded-content').innerHTML = "";
+    liveregion.innerHTML = template;
+
+    setTimeout(() => {
+      document.querySelector('#spinner-icon').setAttribute('aria-label', 'Loaded!')
+      document.querySelector('#loading-text').innerHTML = "Loaded!";
+    }, 2000);
+
+    setTimeout(() => {
+      const request = new XMLHttpRequest();
+      request.open('GET', 'loaded-content.html', true);
+      request.onload = event => {
+        if (request.status >= 200 && request.status < 400) {
+          const resp = request.responseText;
+          document.querySelector('#loaded-content').innerHTML = resp;
+          document.querySelector('#loaded-content title').remove();
+        }
+      };
+      request.send();
+      liveregion.innerHTML = "";
+    }, 2500);
+  }
+
+  window.addEventListener('load',  event => {
+    const button = document.querySelector('#load-page');
+    button.addEventListener('click', showSpinner);
+  });
+
 })();
