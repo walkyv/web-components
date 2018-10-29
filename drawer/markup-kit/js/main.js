2c7c54920ee74d5d693296e36cf73973a66b62d1
Elias Mason <eliasjmason@gmail.com>
2018-10-29 10:00:54 -0700

    Tidy

--- drawer/markup-kit/js/main.js
+++ drawer/markup-kit/js/main.js
@@ -1,6 +1,6 @@
-'use strict';
 
 (function() {
+  'use strict';
   const trigger = document.getElementById('openDrawer'),
     drawer = document.getElementById('drawer'),
     panels = drawer.querySelectorAll('.panel'),
