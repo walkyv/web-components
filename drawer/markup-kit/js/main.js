7f23fa386b20719781963d4cafd21a1dbba53321
Elias Mason <eliasjmason@gmail.com>
2018-10-29 10:13:09 -0700

    Tidy

--- drawer/markup-kit/js/main.js
+++ drawer/markup-kit/js/main.js
@@ -1,4 +1,3 @@
-
 (function() {
   'use strict';
   const trigger = document.getElementById('openDrawer'),
@@ -20,7 +19,7 @@
     Array.prototype.forEach.call(panels, panel => {
       const dataPanel = panel.getAttribute('data-panel');
       if (panelToShow === dataPanel) {
-        show = panel
+        show = panel;
       }
     });
     return show;
@@ -33,21 +32,21 @@
       button.addEventListener('click', event => {
         if (event.currentTarget.classList.contains('back')) {
           closePanel(returnPanel());
-          showPanel(panelOne)
+          showPanel(panelOne);
           drawer.setAttribute('data-current-panel', '1');
         } else {
           closePanel(panelOne);
           closePanel(returnPanel());
         }
-      })
-    })
+      });
+    });
   }
 
   // toggle the panels in ui
   function showPanel(panel) {
     panel.style.right = '0';
     panel.style.display = 'flex';
-    panel.setAttribute('aria-hidden', 'false')
+    panel.setAttribute('aria-hidden', 'false');
     focusTrap(panel);
     mainContent.setAttribute('aria-hidden', 'true');
     drawer.setAttribute('aria-hidden', 'false');
@@ -56,16 +55,16 @@
   function closePanel(panel) {
     panel.style.right = '-320px';
     panel.style.display = 'none';
-    panel.setAttribute('aria-hidden', 'true')
+    panel.setAttribute('aria-hidden', 'true');
     trigger.focus();
     mainContent.setAttribute('aria-hidden', 'false');
-    drawer.setAttribute('aria-hidden', 'true')
+    drawer.setAttribute('aria-hidden', 'true');
   }
 
   function expandPanel(panel) {
     panel.style.right = '320px';
-    panel.style.display = 'none'
-    panel.setAttribute('aria-hidden', 'true')
+    panel.style.display = 'none';
+    panel.setAttribute('aria-hidden', 'true');
   }
 
   // sets focus trap on the panels
@@ -87,27 +86,27 @@
   Array.prototype.forEach.call(questions, question => {
     question.addEventListener('click', event => {
       if (!question.classList.contains('close')) {
-        const showPanelNumber = event.currentTarget.getAttribute('data-show-panel');
+        const showPanelNumber = event.currentTarget.getAttribute(
+          'data-show-panel'
+        );
         expandPanel(returnPanel());
         drawer.setAttribute('data-current-panel', showPanelNumber);
         showPanel(returnPanel());
-        clickHandlers(returnPanel())
+        clickHandlers(returnPanel());
       } else {
-        closePanel(returnPanel())
+        closePanel(returnPanel());
       }
-
-    })
+    });
   });
 
   // opens the panel
   trigger.addEventListener('click', event => {
-    showPanel(returnPanel())
+    showPanel(returnPanel());
   });
 
   document.addEventListener('keyup', event => {
     if (event.code === 'Escape') {
-      console.log('close')
+      console.log('close');
     }
   });
-
 })();
