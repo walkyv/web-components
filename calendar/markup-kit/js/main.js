dfd2e76a0bfc010661a079ab6043f55575b847fc
jackiejakob <jackie.jakob@pearson.com>
2018-11-28 15:55:12 -0800

    Add keyboard functionality for current month view

--- calendar/markup-kit/js/main.js
+++ calendar/markup-kit/js/main.js
@@ -1,6 +1,85 @@
 'use strict';
 (function() {
-    //render based on date pulled from data attribute (or current date)
-    //previous / next arrow render new month
-  // keyboard functionality
+    const calendars = document.querySelectorAll(".pe-calendar");
+
+    calendars.forEach(calendar => {
+        const grid = calendar.querySelector("[role=grid]"),
+            dates = calendar.querySelectorAll("[role=gridcell]:not(:disabled)");
+
+        console.log(dates);
+
+        function unfocusAll() {
+            Array.prototype.forEach.call(dates, function(date) {
+                date.setAttribute("tabindex","-1");
+            });
+        }
+
+        function selectDate(date) {
+            unfocusAll();
+            grid.setAttribute("aria-activedescendant", date.id);
+            date.setAttribute("tabindex","0");
+            date.focus();
+
+            Array.prototype.forEach.call(dates, function(others) {
+                others.removeAttribute("aria-selected");
+
+                if ( others.querySelector(".pe-cal-cell-square").classList.contains("pe-cal-selected") ) {
+                    others.querySelector(".pe-cal-cell-square").classList.remove("pe-cal-selected");
+                }
+            });
+            date.querySelector(".pe-cal-cell-square").classList.add("pe-cal-selected");
+            date.setAttribute("aria-selected","true");
+
+        }
+        //onload, make sure all dates are set to tabindex -1
+        unfocusAll();
+        //set focus to already selected date (if available)
+        var activeDescendant = grid.getAttribute("aria-activedescendant");
+        if (activeDescendant) {
+            document.getElementById(activeDescendant).setAttribute("tabindex","0");
+        }
+
+        grid.addEventListener("keydown", function(event) {
+            var activeDescendant = grid.getAttribute("aria-activedescendant");
+            //get position of active descendant in array
+            var focusedItem = [].indexOf.call(dates,document.getElementById(activeDescendant));
+
+            switch(event.key) {
+                case "ArrowRight":
+                    selectDate( dates[focusedItem + 1]);
+                    break;
+
+                case "ArrowLeft":
+                    selectDate(dates[focusedItem - 1])
+                    break;
+                case "ArrowDown":
+                    selectDate(dates[focusedItem + 7]);
+                    break;
+                case "ArrowUp":
+                    selectDate( dates[focusedItem - 7]);
+                    break;
+                case "PageUp":
+                    //previous month
+                    break;
+                case "PageDown":
+                    //next month
+                    break;
+                case "Home":
+                    //beginning of month
+                    selectDate(dates[0]);
+                    break;
+                case "End":
+                    //end of month
+                    selectDate(dates[dates.length - 1])
+                    break;
+            }
+        })
+
+        dates.forEach(date => {
+            date.addEventListener("click", event => {
+                selectDate(event.currentTarget);
+            })
+        })
+    })
+
 })();
