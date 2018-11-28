ded17e7e084b9a30957bdc094efd2ac890b8ac4e
jackiejakob <jackie.jakob@pearson.com>
2018-11-28 12:03:56 -0800

    Initial markup of calendar, no js

---
+++ calendar/markup-kit/js/dist/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function () {
+    //render based on date pulled from data attribute (or current date)
+    //previous / next arrow render new month
+    // keyboard functionality
+})();
