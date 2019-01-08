a8356ddbeed011613e09852316318ef530bf76a4
David O'Dey <dodey@icloud.com>
2019-01-08 13:46:00 -0700

    first pass at datepicker complete

--- datepicker/markup-kit/dist/js/datepicker.js
+++ datepicker/markup-kit/dist/js/datepicker.js
@@ -1,84 +1,149 @@
-(function () {
+(function (win, doc) {
   'use strict';
 
   var datepickers = document.querySelectorAll('.datepicker-container');
 
-  function unfocusAll(node) {
-    var dates = node.querySelectorAll(".date-selector:not(:disabled)");
-    Array.prototype.forEach.call(dates, function (date) {
-      date.setAttribute("tabindex", "-1");
-    });
+  function closeCalendar(node, input) {
+    node.style.display = 'none';
+    input.setAttribute('aria-expanded', false);
+    input.focus();
   }
 
-  Array.prototype.forEach.call(datepickers, function (datepicker) {
-    var grid = datepicker.querySelector(".pe-cal-dates"),
-        dates = datepicker.querySelectorAll(".date-selector:not(:disabled)"),
-        activeDescendant = grid.getAttribute("aria-activedescendant");
-
-    function focusDate(date) {
-      unfocusAll(datepicker);
-      if (date !== undefined) {
-        grid.setAttribute("aria-activedescendant", date.id);
-        date.setAttribute("tabindex", "0");
-        date.focus();
-      }
+  function openCalendar(node, input) {
+    var selectedDate = input.getAttribute('data-selected');
+    if (selectedDate !== null) {
+      var selectedNode = node.querySelector('[data-date="' + selectedDate + '"]');
+      setTimeout(function () {
+        selectedNode.focus();
+      }, 10);
+    } else {
+      var currentNode = node.querySelector('.currentDate-box button');
+      setTimeout(function () {
+        currentNode.focus();
+      }, 10);
     }
+    node.style.display = 'inline-flex';
+    input.setAttribute('aria-expanded', true);
+  }
 
-    function selectDate(date) {
-      var currentLabel = date.getAttribute("aria-label");
-      focusDate(date);
-      Array.prototype.forEach.call(dates, function (date) {
-        var oldLabel = date.getAttribute("aria-label"),
-            newLabel = oldLabel.replace(" (Selected)", "");
-        date.setAttribute("aria-pressed", "false");
-        if (date.classList.contains("pe-cal-selected")) {
-          date.classList.remove("pe-cal-selected");
-          date.setAttribute("aria-label", newLabel);
-        }
-      });
-      date.classList.add("pe-cal-selected");
-      date.setAttribute("aria-pressed", "true");
-      date.setAttribute("aria-label", currentLabel + " (Selected)");
-    }
+  function selectDate(node, input, dates) {
+    var value = node.getAttribute('data-date');
+    Array.prototype.forEach.call(dates, function (date) {
+      date.classList.remove('selected');
+    });
+    input.setAttribute('data-selected', value);
+    input.value = value;
+    node.classList.add('selected');
+  }
 
-    //onload, make sure all dates are set to tabindex -1
-    unfocusAll(datepicker);
+  Array.prototype.forEach.call(datepickers, function (datepicker, index) {
+    var dates = datepicker.querySelectorAll("button:not(:disabled)"),
+        input = datepicker.querySelector('input'),
+        calendar = datepicker.querySelector('.calendar-container'),
+        focusableElements = dates,
+        firstFocusableElement = focusableElements[0],
+        lastFocusableElement = focusableElements[focusableElements.length - 1];
 
-    //set focus to already selected date (if available)
-    if (activeDescendant) {
-      document.getElementById(activeDescendant).setAttribute("tabindex", "0");
-    }
+    Array.prototype.forEach.call(focusableElements, function (el, index) {
+      el.setAttribute('data-index', index);
+    });
 
-    grid.addEventListener("keydown", function (event) {
+    input.addEventListener('click', function (event) {
+      openCalendar(calendar, input);
+    });
+
+    input.addEventListener('keydown', function (event) {
       event.preventDefault();
-      var activeDescendant = grid.getAttribute("aria-activedescendant"),
-          focusedItem = [].indexOf.call(dates, document.getElementById(activeDescendant));
+      switch (event.keyCode) {
+        case 40:
+          if (calendar.style.display === 'none') {
+            openCalendar(calendar, input);
+          }
+          break;
+      }
+    });
+
+    // sets the date when number is clicked
+    calendar.addEventListener('click', function (event) {
+      if (event.target.classList.contains('date-selector')) {
+        selectDate(event.target, input, dates);
+        closeCalendar(calendar, input);
+      } else {
+        return;
+      }
+    });
+
+    // calendar keyboard navigation
+    calendar.addEventListener("keydown", function (event) {
+      var nextItem = parseInt(event.target.getAttribute('data-index')) + 1,
+          prevItem = parseInt(event.target.getAttribute('data-index')) - 1,
+          nextWeek = parseInt(event.target.getAttribute('data-index')) + 7,
+          prevWeek = parseInt(event.target.getAttribute('data-index')) - 7;
+
+      event.preventDefault();
       switch (event.keyCode) {
         case 39:
-          focusDate(dates[focusedItem + 1]);
+          if (document.activeElement === lastFocusableElement) {
+            firstFocusableElement.focus();
+          } else {
+            if (focusableElements[nextItem] !== undefined) {
+              focusableElements[nextItem].focus();
+            } else {
+              return;
+            }
+          }
           break;
         case 37:
-          focusDate(dates[focusedItem - 1]);
+          if (document.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus();
+          } else {
+            if (focusableElements[prevItem] !== undefined) {
+              focusableElements[prevItem].focus();
+            } else {
+              return;
+            }
+          }
           break;
         case 40:
-          focusDate(dates[focusedItem + 7]);
+          if (focusableElements[nextWeek] !== undefined) {
+            focusableElements[nextWeek].focus();
+          } else {
+            // target next month
+          }
           break;
         case 38:
-          focusDate(dates[focusedItem - 7]);
+          if (focusableElements[prevWeek] !== undefined) {
+            focusableElements[prevWeek].focus();
+          } else {
+            // target previous month
+          }
           break;
-        case 36:
-          focusDate(dates[0]);
-          break;
-        case 35:
-          focusDate(dates[dates.length - 1]);
+        case 13:
+          selectDate(event.target, input, dates);
+          closeCalendar(calendar, input);
           break;
       }
     });
 
-    Array.prototype.forEach.call(dates, function (date) {
-      date.addEventListener("click", function (event) {
-        selectDate(event.currentTarget);
-      });
+    // closes calendar on escape keypress
+    doc.addEventListener('keydown', function (event) {
+      if (event.keyCode === 27) {
+        closeCalendar(calendar, input);
+      }
     });
+
+    // closes dropdown when click is outside of component
+    doc.addEventListener('click', function (event) {
+      if (calendar.style.display === 'inline-flex') {
+        var target = event.target;
+        do {
+          if (target === calendar) {
+            return;
+          }
+          target = target.parentNode;
+        } while (target);
+        closeCalendar(calendar, input);
+      }
+    }, true);
   });
-})();
+})(window, document);
--- datepicker/markup-kit/js/datepicker.js
+++ datepicker/markup-kit/js/datepicker.js
@@ -1,87 +1,150 @@
-(function() {
+(function(win, doc) {
   'use strict';
 
   const datepickers = document.querySelectorAll('.datepicker-container');
 
-  function unfocusAll(node) {
-    const dates = node.querySelectorAll(".date-selector:not(:disabled)");
-    Array.prototype.forEach.call(dates, date => {
-      date.setAttribute("tabindex","-1");
-    });
+  function closeCalendar (node, input) {
+    node.style.display = 'none' ;
+    input.setAttribute('aria-expanded', false)
+    input.focus();
   }
 
-  Array.prototype.forEach.call(datepickers, datepicker => {
-    const grid = datepicker.querySelector(".pe-cal-dates"),
-      dates = datepicker.querySelectorAll(".date-selector:not(:disabled)"),
-      activeDescendant = grid.getAttribute("aria-activedescendant");
-
-    function focusDate(date) {
-      unfocusAll(datepicker);
-      if (date !== undefined) {
-        grid.setAttribute("aria-activedescendant", date.id);
-        date.setAttribute("tabindex","0");
-        date.focus();
-      }
+  function openCalendar (node, input) {
+    const selectedDate = input.getAttribute('data-selected');
+    if (selectedDate !== null) {
+      const selectedNode = node.querySelector(`[data-date="${selectedDate}"]`);
+      setTimeout(() => {
+        selectedNode.focus();
+      },10)
+    } else {
+      const currentNode = node.querySelector('.currentDate-box button');
+      setTimeout(() => {
+        currentNode.focus();
+      },10)
     }
+    node.style.display = 'inline-flex';
+    input.setAttribute('aria-expanded', true);
+  }
 
-    function selectDate(date) {
-      const currentLabel = date.getAttribute("aria-label");
-      focusDate(date);
-      Array.prototype.forEach.call(dates, date =>  {
-        const oldLabel = date.getAttribute("aria-label"),
-          newLabel = oldLabel.replace(" (Selected)", "");
-        date.setAttribute("aria-pressed","false");
-        if ( date.classList.contains("pe-cal-selected") ) {
-          date.classList.remove("pe-cal-selected");
-          date.setAttribute("aria-label", newLabel)
-        }
+  function selectDate(node, input, dates) {
+    const value = node.getAttribute('data-date');
+    Array.prototype.forEach.call(dates, date => {
+      date.classList.remove('selected');
+    });
+    input.setAttribute('data-selected', value);
+    input.value = value;
+    node.classList.add('selected');
+  }
+
+  Array.prototype.forEach.call(datepickers, (datepicker,index) => {
+      const dates = datepicker.querySelectorAll("button:not(:disabled)"),
+      input = datepicker.querySelector('input'),
+      calendar = datepicker.querySelector('.calendar-container'),
+        focusableElements = dates,
+        firstFocusableElement = focusableElements[0],
+        lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+      Array.prototype.forEach.call(focusableElements, (el,index) => {
+        el.setAttribute('data-index', index);
       });
-      date.classList.add("pe-cal-selected");
-      date.setAttribute("aria-pressed","true");
-      date.setAttribute("aria-label", currentLabel + " (Selected)");
-    }
 
+    input.addEventListener('click', event => {
+      openCalendar(calendar, input)
+    });
 
-
-    //onload, make sure all dates are set to tabindex -1
-    unfocusAll(datepicker);
-
-    //set focus to already selected date (if available)
-    if (activeDescendant) {
-      document.getElementById(activeDescendant).setAttribute("tabindex","0");
-    }
-
-    grid.addEventListener("keydown", function(event) {
+    input.addEventListener('keydown', event => {
       event.preventDefault();
-      const activeDescendant = grid.getAttribute("aria-activedescendant"),
-        focusedItem = [].indexOf.call(dates,document.getElementById(activeDescendant));
+      switch (event.keyCode) {
+        case 40:
+          if (calendar.style.display === 'none') {
+            openCalendar(calendar, input)
+          }
+          break
+      }
+    });
+
+    // sets the date when number is clicked
+    calendar.addEventListener('click', event => {
+      if (event.target.classList.contains('date-selector')) {
+        selectDate(event.target, input, dates)
+        closeCalendar(calendar, input)
+      } else {
+        return;
+      }
+    });
+
+    // calendar keyboard navigation
+    calendar.addEventListener("keydown", function(event) {
+      const nextItem = parseInt(event.target.getAttribute('data-index')) + 1,
+        prevItem = parseInt(event.target.getAttribute('data-index')) - 1,
+        nextWeek = parseInt(event.target.getAttribute('data-index')) + 7,
+        prevWeek = parseInt(event.target.getAttribute('data-index')) - 7;
+
+      event.preventDefault();
       switch (event.keyCode) {
         case 39:
-          focusDate( dates[focusedItem + 1]);
+          if (document.activeElement === lastFocusableElement) {
+            firstFocusableElement.focus();
+          } else {
+            if (focusableElements[nextItem] !== undefined) {
+              focusableElements[nextItem].focus();
+            } else {
+              return
+            }
+          }
           break;
         case 37:
-          focusDate(dates[focusedItem - 1])
+          if (document.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus();
+          } else {
+            if (focusableElements[prevItem] !== undefined) {
+              focusableElements[prevItem].focus();
+            } else {
+              return
+            }
+          }
           break;
         case 40:
-          focusDate(dates[focusedItem + 7]);
+          if (focusableElements[nextWeek] !== undefined) {
+            focusableElements[nextWeek].focus();
+          } else {
+            // target next month
+          }
           break;
         case 38:
-          focusDate( dates[focusedItem - 7]);
+          if (focusableElements[prevWeek] !== undefined) {
+            focusableElements[prevWeek].focus();
+          } else {
+            // target previous month
+          }
           break;
-        case 36:
-          focusDate( dates[0]);
-          break;
-        case 35:
-          focusDate( dates[dates.length - 1]);
+        case 13:
+          selectDate(event.target, input, dates);
+          closeCalendar(calendar, input)
           break;
       }
     });
 
-    Array.prototype.forEach.call(dates, date => {
-      date.addEventListener("click", event => {
-        selectDate(event.currentTarget);
-      })
+    // closes calendar on escape keypress
+    doc.addEventListener('keydown', event => {
+      if (event.keyCode === 27) {
+        closeCalendar(calendar, input);
+      }
     });
+
+    // closes dropdown when click is outside of component
+    doc.addEventListener('click', event => {
+      if (calendar.style.display === 'inline-flex') {
+        let target = event.target;
+        do {
+          if (target === calendar)  {
+            return;
+          }
+          target = target.parentNode;
+        } while (target);
+        closeCalendar(calendar, input);
+      }
+    }, true);
   })
 
-})();
+})(window, document);
