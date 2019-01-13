926a0fc543b4948bb71c648b9f6dd6542537c4cd
Elias Mason <eliasjmason@gmail.com>
2019-01-12 20:54:20 -0800

    Misc cleanup

--- progress-indicator/js/dist/progress-indicator.js
+++ progress-indicator/js/dist/progress-indicator.js
@@ -18,7 +18,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
   // and pasted between the style tags below
   alert.innerHTML = '\n        <div role="alert" aria-live="assertive" class="loading-bar-liveregion" id="liveregion">\n          <!-- insert loaded confirmation message here, visually hidden region -->\n        </div>\n  ';
 
-  standard.innerHTML = ' \n    <style>\n    .loading-bar-liveregion{position:absolute!important;height:1px;width:1px;overflow:hidden;clip:rect(1px 1px 1px 1px);clip:rect(1px,1px,1px,1px)}.pe-progress-bar{position:relative;padding-bottom:16px}.pe-progress-bar .fullbar{width:100%;text-align:center;margin:0;line-height:18px;font-size:14px;font-family:Open Sans,Calibri,Tahoma,sans-serif}.pe-progress-bar .fullbar:after{content:"";height:4px;width:100%;position:absolute;bottom:4px;left:0;background-color:#c7c7c7}.pe-progress-bar.left-aligned .fullbar{text-align:left}.pe-progress-bar.right-aligned .fullbar{text-align:right}.pe-progress-bar div[role=progressbar]{height:12px;width:0;position:absolute;left:0;bottom:0;background-color:#19a6a4;-webkit-animation:shift .25s linear infinite;animation:shift .25s linear infinite}.pe-progress-bar.loading div[role=progressbar]{background-image:-webkit-linear-gradient(315deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-image:linear-gradient(135deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-size:18px 18px}@-webkit-keyframes shift{to{background-position:9px 9px}}@keyframes shift{to{background-position:9px 9px}}@media screen and (prefers-reduced-motion:reduce){.pe-progress-bar div[role=progressbar]{-webkit-animation:unset!important;animation:unset!important;-webkit-transition:unset!important;transition:unset!important}}\n    </style>\n     <div class="pe-progress-bar">\n      <p class="fullbar" id="label">0%</p>\n      <div role="progressbar" aria-valuenow="0" aria-valuemax="100" aria-valuemin="0" id="progress"></div>\n  </div>\n';
+  standard.innerHTML = ' \n    <style>\n    .loading-bar-liveregion{position:absolute!important;height:1px;width:1px;overflow:hidden;clip:rect(1px 1px 1px 1px);clip:rect(1px,1px,1px,1px)}.pe-progress-bar{position:relative;padding-bottom:16px}.pe-progress-bar .fullbar{width:100%;text-align:center;margin:0;line-height:18px;font-size:14px;font-family:Open Sans,Calibri,Tahoma,sans-serif}.pe-progress-bar .fullbar:after{content:"";height:4px;width:100%;position:absolute;bottom:4px;left:0;background-color:#c7c7c7}.pe-progress-bar.left-aligned .fullbar{text-align:left}.pe-progress-bar.right-aligned .fullbar{text-align:right}.pe-progress-bar div[role=progressbar]{height:12px;width:0;position:absolute;left:0;bottom:0;background-color:#19a6a4;-webkit-animation:shift .25s linear infinite;animation:shift .25s linear infinite}.pe-progress-bar.loading div[role=progressbar]{background-image:-webkit-linear-gradient(315deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-image:linear-gradient(135deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-size:18px 18px}@-webkit-keyframes shift{to{background-position:9px 9px}}@keyframes shift{to{background-position:9px 9px}}@media screen and (prefers-reduced-motion:reduce){.pe-progress-bar div[role=progressbar]{-webkit-animation:unset!important;animation:unset!important;-webkit-transition:unset!important;transition:unset!important}}\n    </style>\n     <div class="pe-progress-bar">\n      <p class="fullbar" id="label" role="status" aria-live="polite">0%</p>\n      <div role="progressbar" aria-valuenow="0" aria-valuemax="100" aria-valuemin="0" id="progress"></div>\n  </div>\n';
 
   if (w.ShadyCSS) w.ShadyCSS.prepareTemplate(standard, 'progress-indicator');
 
@@ -59,9 +59,9 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
       key: 'progressBar',
       set: function set(progress) {
         this.loadingBar.setAttribute('aria-valuenow', progress);
-        this.loadingBar.setAttribute("style", "width: " + progress + "%;");
+        this.loadingBar.setAttribute('style', 'width: ' + progress + '%;');
         if (this.text === null) {
-          this.label.innerHTML = progress + "%";
+          this.label.innerHTML = progress + '%';
         } else {
           this.label.innerHTML = this.text;
         }
@@ -96,7 +96,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
       _this.attachShadow({ mode: 'open' });
       var clone = standard.content.cloneNode(true);
 
-      _this.loadingBar = clone.querySelector("[role=progressbar]");
+      _this.loadingBar = clone.querySelector('[role=progressbar]');
       _this.wrapper = clone.querySelector('.pe-progress-bar');
       _this.label = clone.querySelector('#label');
 
@@ -115,8 +115,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
         if (this.type === 'loading') {
           this.wrapper.appendChild(alertClone);
-          this.label.setAttribute('aria-live', 'polite');
-          this.label.setAttribute('role', 'status');
         }
       }
     }, {
--- progress-indicator/js/progress-indicator.js
+++ progress-indicator/js/progress-indicator.js
@@ -18,7 +18,7 @@
     .loading-bar-liveregion{position:absolute!important;height:1px;width:1px;overflow:hidden;clip:rect(1px 1px 1px 1px);clip:rect(1px,1px,1px,1px)}.pe-progress-bar{position:relative;padding-bottom:16px}.pe-progress-bar .fullbar{width:100%;text-align:center;margin:0;line-height:18px;font-size:14px;font-family:Open Sans,Calibri,Tahoma,sans-serif}.pe-progress-bar .fullbar:after{content:"";height:4px;width:100%;position:absolute;bottom:4px;left:0;background-color:#c7c7c7}.pe-progress-bar.left-aligned .fullbar{text-align:left}.pe-progress-bar.right-aligned .fullbar{text-align:right}.pe-progress-bar div[role=progressbar]{height:12px;width:0;position:absolute;left:0;bottom:0;background-color:#19a6a4;-webkit-animation:shift .25s linear infinite;animation:shift .25s linear infinite}.pe-progress-bar.loading div[role=progressbar]{background-image:-webkit-linear-gradient(315deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-image:linear-gradient(135deg,#19a6a4 33.33%,#daf0ed 0,#daf0ed 50%,#19a6a4 0,#19a6a4 83.33%,#daf0ed 0,#daf0ed);background-size:18px 18px}@-webkit-keyframes shift{to{background-position:9px 9px}}@keyframes shift{to{background-position:9px 9px}}@media screen and (prefers-reduced-motion:reduce){.pe-progress-bar div[role=progressbar]{-webkit-animation:unset!important;animation:unset!important;-webkit-transition:unset!important;transition:unset!important}}
     </style>
      <div class="pe-progress-bar">
-      <p class="fullbar" id="label">0%</p>
+      <p class="fullbar" id="label" role="status" aria-live="polite">0%</p>
       <div role="progressbar" aria-valuenow="0" aria-valuemax="100" aria-valuemin="0" id="progress"></div>
   </div>
 `;
@@ -49,26 +49,25 @@
       return this.getAttribute('max');
     }
 
-    set titleAlignment (alignment) {
+    set titleAlignment(alignment) {
       this.wrapper.classList.add(alignment);
     }
 
     set progressBar(progress) {
       this.loadingBar.setAttribute('aria-valuenow', progress);
-      this.loadingBar.setAttribute("style", "width: " + progress + "%;");
+      this.loadingBar.setAttribute('style', 'width: ' + progress + '%;');
       if (this.text === null) {
-        this.label.innerHTML = progress + "%";
+        this.label.innerHTML = progress + '%';
       } else {
         this.label.innerHTML = this.text;
       }
+    }
 
-    };
-
-    set loaderType(value){
+    set loaderType(value) {
       this.wrapper.classList.add(value);
     }
 
-    set maxValue(value){
+    set maxValue(value) {
       this.loadingBar.setAttribute('aria-valuemax', value);
     }
 
@@ -81,7 +80,7 @@
       this.attachShadow({ mode: 'open' });
       const clone = standard.content.cloneNode(true);
 
-      this.loadingBar = clone.querySelector("[role=progressbar]");
+      this.loadingBar = clone.querySelector('[role=progressbar]');
       this.wrapper = clone.querySelector('.pe-progress-bar');
       this.label = clone.querySelector('#label');
 
@@ -97,29 +96,26 @@
 
       if (this.type === 'loading') {
         this.wrapper.appendChild(alertClone);
-        this.label.setAttribute('aria-live', 'polite');
-        this.label.setAttribute('role', 'status');
       }
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
       if (name === 'progress') {
-        this.progressBar = newValue
+        this.progressBar = newValue;
         if (this.type === 'loading') {
           if (this.progress === '100') {
-            this.alertMessage = 'Done loading'
+            this.alertMessage = 'Done loading';
             this.dispatchEvent(
               new Event('loaded', {
                 bubbles: true
               })
             );
           } else {
-            this.alertMessage = ' '
+            this.alertMessage = ' ';
           }
         }
       }
     }
-
   }
   customElements.define('progress-indicator', ProgressIndicator);
 })(window, document);
