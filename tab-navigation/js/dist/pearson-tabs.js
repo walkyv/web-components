63ed5e9f23ff88e55a58c515424e2ad1cc084b5a
Elias Mason <eliasjmason@gmail.com>
2019-01-23 21:06:16 -0800

    Strip away code

--- tab-navigation/js/dist/pearson-tabs.js
+++ tab-navigation/js/dist/pearson-tabs.js
@@ -18,7 +18,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
   // Other markup should be written after the closing tag.
 
-  template.innerHTML = ' \n    <style>\n    .pe-tabbed-navigation{position:relative;height:41px}.pe-tabbed-navigation.light{background:#f5f5f5;border-bottom:1px solid #e9e9e9}.pe-tabbed-navigation.dark{background:#005a70;border-bottom:1px solid #005a70}.pe-tabbed-navigation .tab-action{background:none;border:0;border-bottom:3px solid transparent}.pe-tabbed-navigation ul.tab-list{padding:0;margin:0}.pe-tabbed-navigation ul.tab-list>li{list-style-type:none;display:inline-block}.pe-tabbed-navigation li.tab-item.active .tab-action{border-bottom:3px solid transparent;color:#252525}.pe-tabbed-navigation li.tab-item:hover:not(.active) .tab-action,.pe-tabbed-navigation li.tab-item:hover:not(li) .tab-action{border-bottom:3px solid #c7c7c7;color:#252525}.pe-tabbed-navigation .tab-action{padding:.5em 0;margin:.5em 1em;text-decoration:none;cursor:pointer;display:block;color:#6a7070}.pe-tabbed-navigation div{padding:.5em 0;margin:.5em 1em}.pe-tabbed-navigation div button{color:#6a7070}.pe-tabbed-navigation.dark{background-color:#005a70}.pe-tabbed-navigation.dark .tab-action{color:#d9d9d9}.pe-tabbed-navigation.dark li.tab-item.active .tab-action{color:#fff}.pe-tabbed-navigation.dark li.tab-item:hover:not(.active) .tab-action{border-bottom:3px solid #d9d9d9;color:#fff}.pe-tabbed-navigation.dark div button{color:#d9d9d9}.pe-tabbed-navigation .tab-slider{height:3px;background-color:#19a6a4;position:absolute;padding:0;margin:.5em 1em;bottom:-8px;-webkit-transition:all .5s ease-in-out;transition:all .5s ease-in-out}.pe-tab-panels{padding:1em}.hidden{display:none}\n    </style>\n    <slot name="tabs"></slot>\n    <slot name="panels"></slot>\n';
+  template.innerHTML = ' \n    <style>\n    ::slotted(.tabs){position:relative;height:41px;list-style-type:none;padding:0;margin:0}li{color:red}\n    </style>\n    <slot name="tabs"></slot>\n    <slot name="panels"></slot>\n';
 
   if (w.ShadyCSS) w.ShadyCSS.prepareTemplate(template, 'pearson-tabs');
 
@@ -38,49 +38,19 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
       var clone = template.content.cloneNode(true);
 
-      /** If we need references to the children of the component,
-       * we can create them here. If they are created elsewhere,
-       * they will not be available our lifecycle methods.
-       */
-
-      _this.heading = clone.querySelector('#title');
-      _this.button = clone.querySelector('#button');
-
-      /** After all this, we can append our clone to the shadowRoot */
       _this.shadowRoot.appendChild(clone);
-
-      /** We should also bind any event listeners to `this` so their
-       * references do not get lost.
-       */
-      _this.handleClick = _this.handleClick.bind(_this);
       return _this;
     }
 
     _createClass(Tabs, [{
       key: 'connectedCallback',
-      value: function connectedCallback() {
-        /** Any changes to what the component renders should be done here. */
-
-        // Get the attributes set by the consumer
-        var titleText = this.getAttribute('title');
-        var buttonText = this.getAttribute('buttonText');
-
-        this.heading.textContent = titleText;
-        this.button.textContent = buttonText;
-
-        /** Event listeners should also be bound here. */
-        this.button.addEventListener('click', this.handleClick);
-      }
+      value: function connectedCallback() {}
     }, {
       key: 'diconnectedCallback',
-      value: function diconnectedCallback() {
-        this.button.removeEventListener('click', this.handleClick);
-      }
+      value: function diconnectedCallback() {}
     }, {
       key: 'handleClick',
-      value: function handleClick() {
-        this.shadowRoot.appendChild(doc.createTextNode('Shadow button clicked!'));
-      }
+      value: function handleClick() {}
     }]);
 
     return Tabs;
--- tab-navigation/js/pearson-tabs.js
+++ tab-navigation/js/pearson-tabs.js
@@ -11,7 +11,7 @@
 
   template.innerHTML = ` 
     <style>
-    .pe-tabbed-navigation{position:relative;height:41px}.pe-tabbed-navigation.light{background:#f5f5f5;border-bottom:1px solid #e9e9e9}.pe-tabbed-navigation.dark{background:#005a70;border-bottom:1px solid #005a70}.pe-tabbed-navigation .tab-action{background:none;border:0;border-bottom:3px solid transparent}.pe-tabbed-navigation ul.tab-list{padding:0;margin:0}.pe-tabbed-navigation ul.tab-list>li{list-style-type:none;display:inline-block}.pe-tabbed-navigation li.tab-item.active .tab-action{border-bottom:3px solid transparent;color:#252525}.pe-tabbed-navigation li.tab-item:hover:not(.active) .tab-action,.pe-tabbed-navigation li.tab-item:hover:not(li) .tab-action{border-bottom:3px solid #c7c7c7;color:#252525}.pe-tabbed-navigation .tab-action{padding:.5em 0;margin:.5em 1em;text-decoration:none;cursor:pointer;display:block;color:#6a7070}.pe-tabbed-navigation div{padding:.5em 0;margin:.5em 1em}.pe-tabbed-navigation div button{color:#6a7070}.pe-tabbed-navigation.dark{background-color:#005a70}.pe-tabbed-navigation.dark .tab-action{color:#d9d9d9}.pe-tabbed-navigation.dark li.tab-item.active .tab-action{color:#fff}.pe-tabbed-navigation.dark li.tab-item:hover:not(.active) .tab-action{border-bottom:3px solid #d9d9d9;color:#fff}.pe-tabbed-navigation.dark div button{color:#d9d9d9}.pe-tabbed-navigation .tab-slider{height:3px;background-color:#19a6a4;position:absolute;padding:0;margin:.5em 1em;bottom:-8px;-webkit-transition:all .5s ease-in-out;transition:all .5s ease-in-out}.pe-tab-panels{padding:1em}.hidden{display:none}
+    ::slotted(.tabs){position:relative;height:41px;list-style-type:none;padding:0;margin:0}li{color:red}
     </style>
     <slot name="tabs"></slot>
     <slot name="panels"></slot>
@@ -29,45 +29,15 @@
       this.attachShadow({ mode: 'open' });
 
       const clone = template.content.cloneNode(true);
-
-      /** If we need references to the children of the component,
-       * we can create them here. If they are created elsewhere,
-       * they will not be available our lifecycle methods.
-       */
-
-      this.heading = clone.querySelector('#title');
-      this.button = clone.querySelector('#button');
-
-      /** After all this, we can append our clone to the shadowRoot */
+      
       this.shadowRoot.appendChild(clone);
-
-      /** We should also bind any event listeners to `this` so their
-       * references do not get lost.
-       */
-      this.handleClick = this.handleClick.bind(this);
     }
 
-    connectedCallback() {
-      /** Any changes to what the component renders should be done here. */
+    connectedCallback() {}
 
-      // Get the attributes set by the consumer
-      const titleText = this.getAttribute('title');
-      const buttonText = this.getAttribute('buttonText');
+    diconnectedCallback() {}
 
-      this.heading.textContent = titleText;
-      this.button.textContent = buttonText;
-
-      /** Event listeners should also be bound here. */
-      this.button.addEventListener('click', this.handleClick);
-    }
-
-    diconnectedCallback() {
-      this.button.removeEventListener('click', this.handleClick);
-    }
-
-    handleClick() {
-      this.shadowRoot.appendChild(doc.createTextNode('Shadow button clicked!'));
-    }
+    handleClick() {}
   }
   customElements.define('pearson-tabs', Tabs);
 })(window, document);
