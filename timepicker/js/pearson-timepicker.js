f96412d53a5e9ed81cfb21527c96ab5e2ecb4504
David O'Dey <dodey@icloud.com>
2019-01-25 17:08:55 -0700

    changed SVG's from data URI back to SVG

--- timepicker/js/pearson-timepicker.js
+++ timepicker/js/pearson-timepicker.js
@@ -5,9 +5,31 @@
     timeItem = doc.createElement('template'),
     dropdown = doc.createElement('template'),
     isIE11 = !!window.MSInputMethodContext && !!document.documentMode,
-    checkIcon = "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='18' height='18' fill='%236a7070'%3e%3cpath d='M7.35823097,13.99969 C7.08123097,13.99969 6.81423097,13.88469 6.62523097,13.67969 L4.26623097,11.13469 C3.89123097,10.72869 3.91523097,10.09669 4.32023097,9.72169 C4.72423097,9.34469 5.35623097,9.36869 5.73323097,9.77469 L7.25323097,11.41469 L12.182231,4.42369 C12.501231,3.97269 13.125231,3.86469 13.576231,4.18269 C14.027231,4.50069 14.135231,5.12469 13.817231,5.57569 L8.17523097,13.57569 C8.00223097,13.82269 7.72523097,13.97769 7.42423097,13.99769 C7.40223097,13.99869 7.38023097,13.99969 7.35823097,13.99969'/%3e%3c/svg%3e",
-    clockIcon = 'data:image/svg+xml;charset=UTF-8,%3csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 18 18\' width=\'18\' height=\'18\' fill=\'%236a7070\'%3e%3cpath d=\'M13.1035,14.1035 C13.0055,14.2015 12.8775,14.2495 12.7495,14.2495 C12.6225,14.2495 12.4945,14.2015 12.3965,14.1035 L8.1475,9.8545 C8.1005,9.8075 8.0635,9.7525 8.0385,9.6905 C8.0135,9.6295 7.9995,9.5655 7.9995,9.4995 L7.9995,5.4995 C7.9995,5.2235 8.2235,4.9995 8.4995,4.9995 C8.7765,4.9995 8.9995,5.2235 8.9995,5.4995 L8.9995,9.2925 L13.1035,13.3965 C13.2985,13.5915 13.2985,13.9085 13.1035,14.1035 M8.9995,-0.0005 C4.0375,-0.0005 -0.0005,4.0375 -0.0005,8.9995 C-0.0005,13.9625 4.0375,17.9995 8.9995,17.9995 C13.9625,17.9995 17.9995,13.9625 17.9995,8.9995 C17.9995,4.0375 13.9625,-0.0005 8.9995,-0.0005\'/%3e%3c/svg%3e',
-    warningIcon = 'data:image/svg+xml;charset=UTF-8,%3csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 18 18\' width=\'18\' height=\'18\' fill=\'%23db0020\'%3e%3cpath d=\'M9.99994044,10.5 C9.99994044,10.776 9.77594044,11 9.49994044,11 L8.49994044,11 C8.22394044,11 7.99994044,10.776 7.99994044,10.5 L7.99994044,7.5 C7.99994044,7.224 8.22394044,7 8.49994044,7 L9.49994044,7 C9.77594044,7 9.99994044,7.224 9.99994044,7.5 L9.99994044,10.5 Z M9.99994044,13.5 C9.99994044,13.776 9.77594044,14 9.49994044,14 L8.49994044,14 C8.22394044,14 7.99994044,13.776 7.99994044,13.5 L7.99994044,12.5 C7.99994044,12.224 8.22394044,12 8.49994044,12 L9.49994044,12 C9.77594044,12 9.99994044,12.224 9.99994044,12.5 L9.99994044,13.5 Z M15.9460591,15.275 L9.44500379,2.276 C9.26100222,1.908 8.73899778,1.908 8.55499621,2.276 L2.05394089,15.275 C1.88693947,15.607 2.12794152,16 2.49894468,16 L15.5010553,16 C15.8720585,16 16.1130605,15.607 15.9460591,15.275 Z\'/%3e%3c/svg%3e';
+    checkIcon = `    
+      <svg
+      aria-hidden="true"
+      focusable="false"
+      style="width:18px;height:18px;fill:currentColor" 
+      
+    >
+          <path d="M7.35823097,13.99969 C7.08123097,13.99969 6.81423097,13.88469 6.62523097,13.67969 L4.26623097,11.13469 C3.89123097,10.72869 3.91523097,10.09669 4.32023097,9.72169 C4.72423097,9.34469 5.35623097,9.36869 5.73323097,9.77469 L7.25323097,11.41469 L12.182231,4.42369 C12.501231,3.97269 13.125231,3.86469 13.576231,4.18269 C14.027231,4.50069 14.135231,5.12469 13.817231,5.57569 L8.17523097,13.57569 C8.00223097,13.82269 7.72523097,13.97769 7.42423097,13.99769 C7.40223097,13.99869 7.38023097,13.99969 7.35823097,13.99969"/>
+    </svg>`,
+    clockIcon = `    
+      <svg
+      aria-hidden="true"
+      focusable="false"
+       style="width:18px;height:18px;fill:currentColor" 
+    >
+<path d="M13.1035,14.1035 C13.0055,14.2015 12.8775,14.2495 12.7495,14.2495 C12.6225,14.2495 12.4945,14.2015 12.3965,14.1035 L8.1475,9.8545 C8.1005,9.8075 8.0635,9.7525 8.0385,9.6905 C8.0135,9.6295 7.9995,9.5655 7.9995,9.4995 L7.9995,5.4995 C7.9995,5.2235 8.2235,4.9995 8.4995,4.9995 C8.7765,4.9995 8.9995,5.2235 8.9995,5.4995 L8.9995,9.2925 L13.1035,13.3965 C13.2985,13.5915 13.2985,13.9085 13.1035,14.1035 M8.9995,-0.0005 C4.0375,-0.0005 -0.0005,4.0375 -0.0005,8.9995 C-0.0005,13.9625 4.0375,17.9995 8.9995,17.9995 C13.9625,17.9995 17.9995,13.9625 17.9995,8.9995 C17.9995,4.0375 13.9625,-0.0005 8.9995,-0.0005"/>
+    </svg>`,
+    warningIcon = `    
+      <svg
+      aria-hidden="true"
+      focusable="false"
+        style="width:18px;height:18px;fill:currentColor" 
+    >
+          <path d="M9.99994044,10.5 C9.99994044,10.776 9.77594044,11 9.49994044,11 L8.49994044,11 C8.22394044,11 7.99994044,10.776 7.99994044,10.5 L7.99994044,7.5 C7.99994044,7.224 8.22394044,7 8.49994044,7 L9.49994044,7 C9.77594044,7 9.99994044,7.224 9.99994044,7.5 L9.99994044,10.5 Z M9.99994044,13.5 C9.99994044,13.776 9.77594044,14 9.49994044,14 L8.49994044,14 C8.22394044,14 7.99994044,13.776 7.99994044,13.5 L7.99994044,12.5 C7.99994044,12.224 8.22394044,12 8.49994044,12 L9.49994044,12 C9.77594044,12 9.99994044,12.224 9.99994044,12.5 L9.99994044,13.5 Z M15.9460591,15.275 L9.44500379,2.276 C9.26100222,1.908 8.73899778,1.908 8.55499621,2.276 L2.05394089,15.275 C1.88693947,15.607 2.12794152,16 2.49894468,16 L15.5010553,16 C15.8720585,16 16.1130605,15.607 15.9460591,15.275 Z"/>
+    </svg>`;
 
   //Styles must be copied from the css file
   // and pasted between the style tags below
@@ -23,7 +45,7 @@
   timeItem.innerHTML = `
     <li class="item" role="option" tabindex="-1" aria-selected="false" data-time="6:00 AM">
       <span class="pe-icon-wrapper">
-				<img src="${checkIcon}" aria-hidden="true"/>
+				${checkIcon}
       </span>
       <span class="time"></span>
     </li>
@@ -40,11 +62,11 @@
         <div class="timepicker">
             <input type="text" id="timePickerInput" class="pe-textInput--basic" role="combobox" aria-haspopup="true" aria-expanded="false" aria-controls="itemList" aria-owns="itemList" aria-autocomplete="none">
             <span class="pe-icon-wrapper">
-				<img src="${clockIcon}" aria-hidden="true"/>
+				        ${clockIcon}
             </span> 
         </div>
         <p id="inputAriaId" class="pe-input--error_message">
-	         <img src="${warningIcon}" aria-hidden="true"/>
+	         ${warningIcon}
             Enter a valid time.
         </p>
     </div>
@@ -174,6 +196,10 @@
       this.setAttribute('valid', valid)
     }
 
+    closeMenu () {
+      this.openState = 'false'
+      this.list.innerHTML = '';
+    }
     selectTime (node, list) {
       const icon = node.querySelector('.pe-icon-wrapper');
       removeIcons(list);
@@ -183,8 +209,7 @@
       icon.style.display = 'block';
       node.setAttribute('aria-selected', true);
       node.classList.add('in-view');
-      this.openState = 'false'
-      this.list.innerHTML = '';
+      this.closeMenu();
       this.validateTime();
       this.input.focus();
     }
@@ -224,6 +249,7 @@
       }
     }
 
+
     constructor() {
       super();
       this.attachShadow({ mode: 'open' });
@@ -239,7 +265,7 @@
       this.selectTime = this.selectTime.bind(this);
       this.hoverTime = this.hoverTime.bind(this);
       this.validateTime = this.validateTime.bind(this);
-
+      this.closeMenu = this.closeMenu.bind(this)
     }
 
     connectedCallback() {
@@ -324,8 +350,7 @@
         event.stopImmediatePropagation();
         if (this.list.childNodes.length > 1) {
           if (event.target !== this) {
-            this.openState = 'false'
-            this.list.innerHTML = '';
+            this.closeMenu();
           }
         }
       });
@@ -334,6 +359,8 @@
         if (event.keyCode === 27) {
           this.openState = 'false';
           this.input.focus();
+          console.log('hi')
+
         }
       });
 
