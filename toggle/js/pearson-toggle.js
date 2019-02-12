6db2714b8e81d7e69711b09a72057331498406cd
Elias Mason <eliasjmason@gmail.com>
2019-02-12 13:47:34 -0800

    remove _ prefix from fns

--- toggle/js/pearson-toggle.js
+++ toggle/js/pearson-toggle.js
@@ -1,7 +1,7 @@
 (function(w, doc) {
   'use strict';
 
-  const SVG_CHECK_SM_18 = `
+  const SVGCHECKSM18 = `
     <svg
       class="pe-icon--check-sm-18"
       aria-hidden="true"
@@ -14,7 +14,7 @@
   </svg>
 `;
 
-  const SVG_REMOVE_SM_18 = `
+  const SVGREMOVESM18 = `
     <svg
       class="pe-icon--remove-sm-18"
       aria-hidden="true"
@@ -33,8 +33,8 @@
   <style>
   *,:after,:before{-webkit-box-sizing:border-box;box-sizing:border-box}:host{font-size:24px;position:relative;display:block;width:2em;height:1em;padding:0;border:0;border-radius:22px;margin:4px 0;color:#fff;background-color:#a9a9a9;cursor:pointer;-webkit-transition:background .3s;transition:background .3s;overflow:visible}:host([on]){background:#047a9c}:host([disabled]){color:#c7c7c7;background-color:#e9e9e9;cursor:not-allowed}:host:before{content:"";background:#fff;display:block;position:absolute;left:3px;top:50%;-webkit-transform:translateY(-50%);transform:translateY(-50%);width:.75em;height:.75em;-webkit-transition:left .3s;transition:left .3s;border-radius:100%}:host([on]):before{left:27px}:host([disabled]):before{background-color:#c7c7c7}:host(:focus){outline:0}:host(:focus):after{content:"";position:absolute;left:50%;top:50%;width:-webkit-calc(100% + 8px);width:calc(100% + 8px);height:-webkit-calc(100% + 8px);height:calc(100% + 8px);border:2px solid #1977d4;border-radius:22px;-webkit-transform:translate(-50%,-50%);transform:translate(-50%,-50%);z-index:1}svg{width:.75em;height:.75em;position:absolute;top:50%;-webkit-transform:translateY(-50%);transform:translateY(-50%);fill:currentColor;pointer-events:none}svg[class*=check]{left:5px}svg[class*=remove]{right:5px}:host(:not([on])) svg[class*=check],:host([on]) svg[class*=remove]{display:none}@media (prefers-reduced-motion:reduce){:host,:host:before{-webkit-transition:none!important;transition:none!important}}
 	</style>
-  ${SVG_CHECK_SM_18}
-  ${SVG_REMOVE_SM_18}
+  ${SVGCHECKSM18}
+  ${SVGREMOVESM18}
   `;
 
   if (w.ShadyCSS) w.ShadyCSS.prepareTemplate(template, 'pearson-toggle');
@@ -58,12 +58,12 @@
 
       this.shadowRoot.appendChild(clone);
 
-      this._onToggleClick = this._onToggleClick.bind(this);
-      this._onToggleKeyUp = this._onToggleKeyUp.bind(this);
+      this.onToggleClick = this.onToggleClick.bind(this);
+      this.onToggleKeyUp = this.onToggleKeyUp.bind(this);
 
-      this._onLabelClick = this._onLabelClick.bind(this);
+      this.onLabelClick = this.onLabelClick.bind(this);
 
-      this._onDOMLoaded = this._onDOMLoaded.bind(this);
+      this.onDOMLoaded = this.onDOMLoaded.bind(this);
     }
 
     connectedCallback() {
@@ -77,37 +77,37 @@
 
       // Lazily upgrade properties to make sure
       // observed attributes are handled properly
-      this._upgradeProperty('on');
-      this._upgradeProperty('disabled');
+      this.upgradeProperty('on');
+      this.upgradeProperty('disabled');
 
       // Bind listeners to the toggle
-      this.addEventListener('click', this._onToggleClick);
-      this.addEventListener('keyup', this._onToggleKeyUp);
+      this.addEventListener('click', this.onToggleClick);
+      this.addEventListener('keyup', this.onToggleKeyUp);
 
       // If the consumer did not set an `aria-label`,
       if (!this.hasAttribute('aria-label')) {
         // We need to find an external one
-        doc.addEventListener('DOMContentLoaded', this._onDOMLoaded, true);
+        doc.addEventListener('DOMContentLoaded', this.onDOMLoaded, true);
       }
     }
 
-    _onToggleClick(e) {
+    onToggleClick(e) {
       e.stopPropagation();
-      this._toggleOn();
+      this.toggleOn();
     }
 
-    _onToggleKeyUp(e) {
+    onToggleKeyUp(e) {
       if (e.altKey) {
         return;
       }
 
       if (e.keyCode === KEYCODE.SPACE || e.keyCode === KEYCODE.ENTER) {
         e.preventDefault();
-        this._toggleOn();
+        this.toggleOn();
       }
     }
 
-    _upgradeProperty(prop) {
+    upgradeProperty(prop) {
       if (this.hasOwnProperty(prop)) {
         let value = this[prop];
         delete this[prop];
@@ -115,7 +115,7 @@
       }
     }
 
-    _toggleOn() {
+    toggleOn() {
       if (this.disabled) return;
 
       this.on = !this.on;
@@ -133,7 +133,7 @@
     }
 
     // Helper function for finding external label node
-    _findLabelNode() {
+    findLabelNode() {
       if (this.parentElement.tagName === 'LABEL') {
         return this.parentElement;
       }
@@ -143,7 +143,7 @@
 
     // When this label is clicked, we want to
     // click on this toggle and focus on it
-    _onLabelClick(e) {
+    onLabelClick(e) {
       e.preventDefault();
 
       if (this.disabled) return;
@@ -152,18 +152,18 @@
       this.focus();
     }
 
-    _onDOMLoaded() {
-      this.labelNode = this._findLabelNode();
+    onDOMLoaded() {
+      this.labelNode = this.findLabelNode();
 
       // If the external label does not have an ID, we must
       // ensure that it has one
-      if (!this.labelNode.id) this.labelNode.id = this.id + '_label';
+      if (!this.labelNode.id) this.labelNode.id = this.id + 'label';
 
       // This toggle must be labelled by the external label node
       this.setAttribute('aria-labelledby', this.labelNode.id);
 
       // We listen for the external label to be clicked
-      this.labelNode.addEventListener('click', this._onLabelClick);
+      this.labelNode.addEventListener('click', this.onLabelClick);
     }
 
     get on() {
@@ -217,13 +217,13 @@
     }
 
     disconnectedCallback() {
-      this.removeEventListener('click', this._onToggleClick);
-      this.removeEventListener('keyup', this._onToggleKeyUp);
+      this.removeEventListener('click', this.onToggleClick);
+      this.removeEventListener('keyup', this.onToggleKeyUp);
 
-      doc.removeEventListener('DOMContentLoaded', this._onDOMLoaded);
+      doc.removeEventListener('DOMContentLoaded', this.onDOMLoaded);
 
       if (this.labelNode) {
-        this.labelNode.removeEventListener('click', this._onLabelClick);
+        this.labelNode.removeEventListener('click', this.onLabelClick);
       }
     }
   }
