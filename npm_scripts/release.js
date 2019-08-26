84b323a2e4eb832ef989912c50b0da81e432e4bc
David O'Dey <dodey@icloud.com>
2019-08-26 12:34:25 -0700

    release

--- npm_scripts/release.js
+++ npm_scripts/release.js
@@ -5,9 +5,9 @@ const exec = require('./exec');
 const path = require('path');
 const readline = require('readline');
 const semver = require('semver');
-const parentPkg = require('../build/package.json');
+
 const branchName = exec('git rev-parse --abbrev-ref HEAD', true);
-const currentVersion = parentPkg.version;
+
 const stdin = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -21,6 +21,7 @@ const syncRemote = (branchName, nextVersion) => {
     log.secondary(`TravisCI will now release to npm on the tagged commit ${nextVersion} for the pearson-ux account.`);
   }
 };
+
 const exitFailure = (message) => {
   log.primaryError(message);
   process.exit(1);
@@ -30,34 +31,58 @@ if (branchName !== 'master' && branchName.toLowerCase().charAt(0) !== 'v') {
     exitFailure('You must be on the master branch in order to execute a release.');
 }
 
-// *** Releaser provides the target SEMVER-compliant version ***
-stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+stdin.question(`Please enter the folder name of the component you want to release `, (component) => {
+  const parentPkg = require(`../${component}/package.json`),
+    currentVersion = parentPkg.version;
 
-  if (!semver.valid(nextVersion)) {
-    exitFailure(`Version '${nextVersion}' is not valid: requires a semver-compliant version. See http://semver.org/`);
-  }
+  stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+    if (!semver.valid(nextVersion)) {
+      exitFailure(`Version '${nextVersion}' is not valid: requires a semver-compliant version. See http://semver.org/`);
+    }
+    if (!semver.gt(nextVersion, currentVersion)) {
+      exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
+    }
+    if (nextVersion.startsWith('v')) {
+      nextVersion = nextVersion.slice(1);
+    }
 
-  if (!semver.gt(nextVersion, currentVersion)) {
-    exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
-  }
 
-  if (nextVersion.startsWith('v')) {
-    nextVersion = nextVersion.slice(1);
-  }
+    exec(`cd ./${component} && gulp build`);
+    exec(`npm version ${nextVersion}`);
+    exec(`cd ../ && git add .`);
+    exec(`git commit -m "releasing ${nextVersion}"`);
+    exec(`cd ././${component} && npm publish`);
+    // exec('gulp publish')
 
-  // Order of operations:
-  // 1. Bump the version update in package.json and npm-shrinkwrap.json
-  // 2. The 'version' custom npm script (defined in package.json) executes changelog generation and adding to commit
-  // 3. Locally commit and tag
-  exec(`gulp build`);
-  exec(`npm version ${nextVersion}`);
-  exec(`cd ./build && npm version ${nextVersion}`);
-  exec(`git add . && cd ../`);
-  exec(`git commit -m "releasing ${nextVersion}"`);
-  exec(`cd ./build && npm publish`);
-  exec('gulp publish')
   // push commit and tag on target release branch
-  syncRemote(branchName, nextVersion);
-
-  stdin.close();
+    syncRemote(branchName, nextVersion);
+    stdin.close();
+  });
 });
+// *** Releaser provides the target SEMVER-compliant version ***
+// stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+//   if (!semver.valid(nextVersion)) {
+//     exitFailure(`Version '${nextVersion}' is not valid: requires a semver-compliant version. See http://semver.org/`);
+//   }
+//   if (!semver.gt(nextVersion, currentVersion)) {
+//     exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
+//   }
+//   if (nextVersion.startsWith('v')) {
+//     nextVersion = nextVersion.slice(1);
+//   }
+//   // Order of operations:
+//   // 1. Bump the version update in package.json and npm-shrinkwrap.json
+//   // 2. The 'version' custom npm script (defined in package.json) executes changelog generation and adding to commit
+//   // 3. Locally commit and tag
+//   exec(`gulp build`);
+//   exec(`npm version ${nextVersion}`);
+//   exec(`cd ./build && npm version ${nextVersion}`);
+//   exec(`git add . && cd ../`);
+//   exec(`git commit -m "releasing ${nextVersion}"`);
+//   exec(`cd ./build && npm publish`);
+//   exec('gulp publish')
+//   // push commit and tag on target release branch
+//   syncRemote(branchName, nextVersion);
+//
+//   stdin.close();
+// });
