029d6e5d1181f4f4e8886a96172c19b3ccbde01b
David O'Dey <dodey@icloud.com>
2019-08-26 09:52:05 -0700

    adding release script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const concat = require('gulp-concat'),
   gulp = require('gulp'),
-  minify = require('gulp-minify');
+  minify = require('gulp-minify'),
+  awspublish = require("gulp-awspublish"),
+  path   = require('path'),
+  rename = require('gulp-rename'),
+  NEW_S3_DIRECTORY = 'components';
 
 // Make a collection of paths used by the various
 // build steps
@@ -44,3 +48,34 @@ function scripts(done) {
 const build = gulp.series(scripts);
 exports.build = build;
 exports.default = gulp.series(build);
+
+gulp.task("publish", function() {
+  var publisher = awspublish.create(
+    {
+      region: "sfo2",
+      endpoint: "sfo2.digitaloceanspaces.com",
+      params: {
+        Bucket: "pearsonux"
+      },
+      accessKeyId: process.env.S3_KEY,
+      secretAccessKey: process.env.S3_SECRET
+    }
+  );
+
+  const headers = {
+    "Cache-Control": "max-age=315360000, no-transform, public"
+  };
+
+  return (
+    gulp
+    .src("./build/pearson-web-components-min.js")
+    .pipe(rename(function(filePath) {
+      filePath.dirname = path.join(NEW_S3_DIRECTORY, filePath.dirname);
+    }))
+    .pipe(awspublish.gzip({ ext: ".gz" }))
+    .pipe(publisher.publish(headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter())
+  );
+});
+
---
+++ npm_scripts/release.js
@@ -0,0 +1,63 @@
+// Execute this script in the target branch to release to npm!
+
+const log = require('./log');
+const exec = require('./exec');
+const path = require('path');
+const readline = require('readline');
+const semver = require('semver');
+const parentPkg = require('../source/css/scss/gravity/package.json');
+const branchName = exec('git rev-parse --abbrev-ref HEAD', true);
+const currentVersion = parentPkg.version;
+const stdin = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const syncRemote = (branchName, nextVersion) => {
+  exec(`git push origin ${branchName}`);
+
+  if (nextVersion) {
+    exec(`git push --tags`);
+    log.secondary(`TravisCI will now release to npm on the tagged commit ${nextVersion} for the pearson-ux account.`);
+  }
+};
+const exitFailure = (message) => {
+  log.primaryError(message);
+  process.exit(1);
+};
+
+if (branchName !== 'master' && branchName.toLowerCase().charAt(0) !== 'v') {
+    exitFailure('You must be on the master branch in order to execute a release.');
+}
+
+// *** Releaser provides the target SEMVER-compliant version ***
+stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+
+  if (!semver.valid(nextVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: requires a semver-compliant version. See http://semver.org/`);
+  }
+
+  if (!semver.gt(nextVersion, currentVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
+  }
+
+  if (nextVersion.startsWith('v')) {
+    nextVersion = nextVersion.slice(1);
+  }
+
+  // Order of operations:
+  // 1. Bump the version update in package.json and npm-shrinkwrap.json
+  // 2. The 'version' custom npm script (defined in package.json) executes changelog generation and adding to commit
+  // 3. Locally commit and tag
+  exec(`gulp build`);
+  exec(`npm version ${nextVersion}`);
+  exec(`cd ./build && npm version ${nextVersion}`);
+  exec(`git add .`);
+  exec(`git commit -m "releasing ${nextVersion}"`);
+  exec(`cd ./build && npm publish`);
+  exec('gulp publish')
+  // push commit and tag on target release branch
+  syncRemote(branchName, nextVersion);
+
+  stdin.close();
+});
