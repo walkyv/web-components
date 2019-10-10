c2c9d9c225cba3ae6f91696687b01e552da7bfa1
David O'Dey <dodey@icloud.com>
2019-10-10 16:19:43 -0700

    changed z-index on modal

--- modal/gulpfile.js
+++ modal/gulpfile.js
@@ -73,6 +73,7 @@ const build = gulp.series(styles, scripts);
 exports.build = build;
 exports.serve = serve;
 exports.watch = watch;
+exports.styles = styles;
 exports.scripts = scripts;
 exports.default = gulp.series(build, serve, watch);
 
